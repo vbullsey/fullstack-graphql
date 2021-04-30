49c8a77ed0310a935f6a8dc802b94cb5b03c4f45
Felipe Roman <felipe.roman@2brains.cl>
2021-04-30 13:09:54 -0400

    jest integration with styled componentes and settings

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,14 @@
+import * as S from './styles'
+
+const Main = () => (
+  <S.Wrapper>
+    <S.Logo src="/img/logo.svg" />
+    <S.Title>React Avanzado</S.Title>
+    <S.Description>
+      React, Typescript, NextJS and Styled Components
+    </S.Description>
+    <S.Illustration src="/img/hero-illustration.svg" alt="Coding" />
+  </S.Wrapper>
+)
+
+export default Main
