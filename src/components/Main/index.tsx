ae6b2c6e9f49cb95b16d00f2501cf5580a42f08e
Felipe Roman <felipe.roman@2brains.cl>
2021-04-30 17:12:04 -0400

    storybook implemented

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,12 +1,19 @@
+import { FC } from 'react'
 import * as S from './styles'
 
-const Main = () => (
+interface iProps {
+  title?: string
+  description?: string
+}
+
+const Main: FC<iProps> = ({
+  title = 'React Avanzado',
+  description = 'Typescript, React, NextJS and Styled Components'
+}) => (
   <S.Wrapper>
     <S.Logo src="/img/logo.svg" />
-    <S.Title>React Avanzado</S.Title>
-    <S.Description>
-      React, Typescript, NextJS and Styled Components
-    </S.Description>
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
     <S.Illustration src="/img/hero-illustration.svg" alt="Coding" />
   </S.Wrapper>
 )
